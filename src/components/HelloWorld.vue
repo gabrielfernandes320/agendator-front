0e35889bc589324963ef2c5827b138fb4c5fc5a9
Gabriel <gabrielfernandesfg@hotmail.com>
2019-11-11 02:48:09 -0300

    fisrt commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Cards />
+  </div>
+</template>
+
+<script>
+import Cards from "./Cards.vue";
+export default {
+  name: "cards",
+  components: {
+    Cards
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+<style scoped>
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,84 @@
+<template>
+  <sidebar-menu :menu="menu" />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      menu: [
+        {
+          header: true,
+          title: "Main Navigation",
+          hiddenOnCollapse: true
+        },
+        {
+          href: "/",
+          title: "Dashboard",
+          icon: "fa fa-user"
+        },
+        {
+          href: "/charts",
+          title: "Charts",
+          icon: "fa fa-chart-area",
+          child: [
+            {
+              href: "/charts/sublink",
+              title: "Sub Link"
+            }
+          ]
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+.v-sidebar-menu {
+}
+.v-sidebar-menu.vsm_expanded {
+}
+.v-sidebar-menu.vsm_collapsed {
+}
+.v-sidebar-menu.vsm_rtl {
+}
+.v-sidebar-menu .vsm--item {
+}
+.v-sidebar-menu .vsm--item.vsm--item_open {
+}
+.v-sidebar-menu .vsm--link {
+}
+.v-sidebar-menu .vsm--link.vsm--link_active {
+}
+.v-sidebar-menu .vsm--link.vsm--link_exact-active {
+}
+.v-sidebar-menu .vsm--link.vsm--link_mobile-item {
+}
+.v-sidebar-menu .vsm--link.vsm--link_level-[n] {
+}
+.v-sidebar-menu .vsm--link.vsm--link_disabled {
+}
+.v-sidebar-menu .vsm--title {
+}
+.v-sidebar-menu .vsm--icon {
+}
+.v-sidebar-menu .vsm--arrow {
+}
+.v-sidebar-menu .vsm--arrow.vsm--arrow_open {
+}
+.v-sidebar-menu .vsm--badge {
+}
+.v-sidebar-menu .vsm--header {
+}
+.v-sidebar-menu .vsm--list {
+}
+.v-sidebar-menu .vsm--dropdown > .vsm--list {
+}
+.v-sidebar-menu .vsm--mobile-item {
+}
+.v-sidebar-menu .vsm--mobile-bg {
+}
+.v-sidebar-menu .vsm--toggle-btn {
+}
+</style>
